df90f38d714ca92f6a13042c9a465293e986e21d
Anumoy Murmu <103399542+AnumoyMurmu@users.noreply.github.com>
2024-01-29 20:36:08 +0530

    Add files via upload

---
+++ Searching_an_element_in_a_sortedArray.cpp
@@ -0,0 +1,25 @@
+
+
+class Solution{
+    public:
+    // Function to find element in sorted array
+    // arr: input array
+    // N: size of array
+    // K: element to be searche
+    int searchInSorted(int arr[], int N, int K) 
+    { 
+    
+       // Your code here
+       int low=0, high=N-1;
+       while(low<=high){
+           long long mid=(low+high)/2;
+           
+           if(arr[mid]==K) return 1;
+           else if(K>arr[mid]) low=mid+1;
+           else high=mid-1;
+       }
+       
+       return -1;
+       
+    }
+};
