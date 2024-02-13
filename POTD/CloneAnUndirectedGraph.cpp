bf0e824f577aa6fa6b85950afad085f13022aa7e
Anumoy Murmu <103399542+AnumoyMurmu@users.noreply.github.com>
2024-02-13 20:38:05 +0530

    Update POTD

---
+++ POTD/CloneAnUndirectedGraph.cpp
@@ -0,0 +1,46 @@
+
+
+//User function Template for C++
+
+// struct Node {
+//     int val;
+//     vector<Node*> neighbors;
+//     Node() {
+//         val = 0;
+//         neighbors = vector<Node*>();
+//     }
+//     Node(int _val) {
+//         val = _val;
+//         neighbors = vector<Node*>();
+//     }
+//     Node(int _val, vector<Node*> _neighbors) {
+//         val = _val;
+//         neighbors = _neighbors;
+//     }
+// };
+
+class Solution {
+public:
+Node* ansNode(Node* node,unordered_map<Node*,Node*> &visited){
+        if (!node) return nullptr;
+        if (visited.find(node) != visited.end())
+            return visited[node];
+        Node* cloneNode = new Node(node->val);
+        visited[node] = cloneNode;
+        for (Node* neighbor : node->neighbors) {
+            cloneNode->neighbors.push_back(ansNode(neighbor, visited));
+        }
+        
+        return cloneNode;
+    
+    }
+    Node* cloneGraph(Node* node) {
+        
+          if (!node) return nullptr;
+        unordered_map<Node*, Node*> visited;
+        
+        return ansNode(node,visited );
+    }
+};
+
+
