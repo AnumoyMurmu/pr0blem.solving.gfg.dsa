ea90606c468ae96915f2b4d6eee9f80d221ef0d4
Anumoy Murmu <103399542+AnumoyMurmu@users.noreply.github.com>
2024-03-07 21:46:21 +0530

    Update PaintAlternate.cpp

--- PaintAlternate.cpp
+++ PaintAlternate.cpp
@@ -28,3 +28,19 @@
 // 1 <= Ai <= 105
 
 // soln:
+
+//User function Template for C++
+
+// ar[] is the array 
+// n is the number of elements in array.
+void print(int ar[], int n)
+{
+    
+    // code here
+       
+           for(int i = 0; i < n; i++){
+        cout << ar[i]<< " ";
+        i++;
+    }
+    
+}
