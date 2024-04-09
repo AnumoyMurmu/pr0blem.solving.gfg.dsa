f7de026ea4eff5c08b3a113146453ea95f658c78
Anumoy Murmu <103399542+AnumoyMurmu@users.noreply.github.com>
2024-04-09 22:48:02 +0530

    Update PalindromicArray.py

--- Array/PalindromicArray.py
+++ Array/PalindromicArray.py
@@ -1,1 +1,7 @@
+# Your task is to complete this function
+# Function should return True/False or 1/0
+def PalinArray(arr ,n):
+    # Code here
+     return all(str(w) == str(w)[::-1] for w in arr)
+
 
