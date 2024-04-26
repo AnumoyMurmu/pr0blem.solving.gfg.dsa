820406aa506f475a5288f110664b09b0e651bdfb
Anumoy Murmu <103399542+AnumoyMurmu@users.noreply.github.com>
2024-04-26 22:36:00 +0530

    Create CheckVowelOrNot.py

---
+++ String/CheckVowelOrNot.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+class Solution:
+    def isVowel (ob,c):
+        # code here 
+         if c in list('aeiouAEIOU'):
+            return "YES"
+         return "NO"
