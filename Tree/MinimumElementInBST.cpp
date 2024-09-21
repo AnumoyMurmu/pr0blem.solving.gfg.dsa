e2260e821175593a7877f0dccad276ef6dadd86a
Anumoy Murmu <103399542+AnumoyMurmu@users.noreply.github.com>
2024-09-21 22:04:15 +0530

    Add files via upload

---
+++ Tree/MinimumElementInBST.cpp
@@ -0,0 +1,34 @@
+// User function Template for C++
+
+// Function to find the minimum element in the given BST.
+
+/* struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+*/
+
+class Solution {
+  public:
+    int minValue(Node* root) {
+        // Code here
+        
+        Node* curr=root;
+        
+        while(curr->left!=NULL){
+            curr=curr->left;
+        }
+        
+        return curr->data;
+    }
+};
