6e8ded0fc72298d991c001f7bd163715b27fce9a
Anumoy Murmu <103399542+AnumoyMurmu@users.noreply.github.com>
2024-09-23 12:01:55 +0530

    Update SizeOfBinaryTree.cpp

--- Tree/SizeOfBinaryTree.cpp
+++ Tree/SizeOfBinaryTree.cpp
@@ -47,3 +47,32 @@ Constraints:
 */
 
 // soln:
+/*
+
+Definition for Binary Tree Node
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int getSize(Node* node) {
+        // code here
+        
+        if(node==NULL) return 0;
+        
+        int left=getSize(node->left);
+        int right=getSize(node->right);
+        
+        return left+right+1;
+    }
+};
