8fe4d15676adf6ae2b39d2c5c4003861983d3def
Anumoy Murmu <103399542+AnumoyMurmu@users.noreply.github.com>
2024-09-24 23:18:36 +0530

    Update PreorderTraversal.cpp

--- Tree/PreorderTraversal.cpp
+++ Tree/PreorderTraversal.cpp
@@ -1,1 +1,29 @@
-
+vector <int> preorder(Node* root)
+{
+  // Your code here
+  vector<int>pre;
+  Node*cur=root;
+  while(cur!=NULL){
+      if(cur->left==NULL){
+          pre.push_back(cur->data);
+          cur=cur->right;
+      }
+      else{
+          Node*prev=cur->left;
+          while(prev->right && prev->right!=cur ){
+              prev=prev->right;
+          }
+          if(prev->right==NULL){
+              prev->right=cur;
+              pre.push_back(cur->data);
+              cur=cur->left;
+          }
+          else{
+              prev->right=NULL;
+              cur=cur->right;
+      }
+  }
+}
+return pre;
+  
+}
