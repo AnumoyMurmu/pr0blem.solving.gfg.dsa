e4364cdd288d8a3b1394ee86949f2cc707f45a43
Anumoy Murmu <103399542+AnumoyMurmu@users.noreply.github.com>
2024-10-13 09:38:10 +0530

    Update MaximumOccuringCharacter.cpp

--- String/MaximumOccuringCharacter.cpp
+++ String/MaximumOccuringCharacter.cpp
@@ -3,3 +3,32 @@
 // If more than one character occurs the maximum number of time then print the lexicographically smaller character.
 
 
+
+
+class Solution
+{
+    public:
+    //Function to find the maximum occurring character in a string.
+    char getMaxOccuringChar(string str)
+    {
+        // Your code here
+        
+        map<char, int>mpp;
+        
+        for(int i=0; i<str.length(); i++){
+            mpp[str[i]]++;
+        }
+        
+        int mx=0, ans;
+        
+        for(auto it:mpp){
+            if(it.second>mx){
+                ans=it.first;
+                mx=it.second;
+            }
+        }
+        return ans;
+        
+    }
+
+};
