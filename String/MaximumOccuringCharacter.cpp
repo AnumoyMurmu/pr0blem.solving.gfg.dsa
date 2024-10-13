abbedf77a78d9323028a46cba1b8c6f7e677244c
Anumoy Murmu <103399542+AnumoyMurmu@users.noreply.github.com>
2024-10-13 09:37:56 +0530

    Update MaximumOccuringCharacter.cpp

--- String/MaximumOccuringCharacter.cpp
+++ String/MaximumOccuringCharacter.cpp
@@ -1,1 +1,5 @@
+// Given a string str of lowercase alphabets. 
+  // The task is to find the maximum occurring character in the string str. 
+// If more than one character occurs the maximum number of time then print the lexicographically smaller character.
+
 
